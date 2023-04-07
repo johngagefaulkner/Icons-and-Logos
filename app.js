f0e8ad8ebbcca6c1b6c50d3527ea88b28297e1dd
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2023-04-07 01:23:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 const fetchData = async () => {
-    try {
-        const response = await fetch("https://raw.githubusercontent.com/johngagefaulkner/Icons-and-Logos/main/SuiteUX-Shell-AppIcons.json");
-        const data = await response.json();
-        return data["SuiteUX-Shell-AppIcons"];
-    } catch (error) {
-        console.error("Error fetching data:", error);
-    }
+  try {
+    const response = await fetch("https://raw.githubusercontent.com/johngagefaulkner/Icons-and-Logos/main/SuiteUX-Shell-AppIcons.json");
+    const data = await response.json();
+    return data["SuiteUX-Shell-AppIcons"];
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 };
 
 const displayImages = (images) => {
-    const imageContainer = document.getElementById("image-container");
-    images.forEach((image) => {
-        const imageCard = document.createElement("div");
-        imageCard.className = "image-card";
+  const imageContainer = document.getElementById("image-container");
+  images.forEach((image) => {
+    const imageCard = document.createElement("div");
+    imageCard.className = "image-card";
 
-        const img = document.createElement("img");
-        img.src = image.Data;
-        img.alt = `Image ${image.Id}`;
-        img.style.width = "100%";
+    const img = document.createElement("img");
+    img.src = image.Data;
+    img.alt = `Image ${image.Id}`;
+    img.style.width = "100%";
 
-        imageCard.appendChild(img);
-        imageContainer.appendChild(imageCard);
-    });
+    const imageTitle = document.createElement("div");
+    imageTitle.className = "image-title";
+    imageTitle.innerText = image.Id;
+
+    imageCard.appendChild(img);
+    imageCard.appendChild(imageTitle);
+    imageContainer.appendChild(imageCard);
+  });
 };
 
 (async () => {
-    const images = await fetchData();
-    displayImages(images);
+  const images = await fetchData();
+  displayImages(images);
 })();
