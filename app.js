47a6cf07ec7c500e6c70a8c2295286ff6ca296e4
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2024-04-25 02:29:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,29 @@
 const fetchData = async () => {
-  try {
-    const response = await fetch("https://raw.githubusercontent.com/johngagefaulkner/Icons-and-Logos/main/SuiteUX-Shell-AppIcons.json");
-    const data = await response.json();
-    return data["SuiteUX-Shell-AppIcons"];
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
+    try {
+        const response = await fetch("https://api.github.com/repos/johngagefaulkner/Icons-and-Logos/contents/src/svg/Microsoft/2024-microsoft-365-content-icons?ref=main");
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
 };
 
 const displayImages = (images) => {
-  const imageContainer = document.getElementById("image-container");
-  images.forEach((image) => {
-    const imageCard = document.createElement("div");
-    imageCard.className = "image-card";
+    const imageContainer = document.getElementById("image-container");
+    images.forEach((image) => {
+        const imageCard = document.createElement("div");
+        imageCard.className = "image-card";
 
-    const img = document.createElement("img");
-    img.src = image.Data;
-    img.alt = `Image ${image.Id}`;
-    img.style.width = "100%";
+        const img = document.createElement("img");
+        img.src = image.download_url
+        img.style.width = "100%";
 
-    const imageTitle = document.createElement("div");
-    imageTitle.className = "image-title";
-    imageTitle.innerText = image.Id;
-
-    imageCard.appendChild(img);
-    imageCard.appendChild(imageTitle);
-    imageContainer.appendChild(imageCard);
-  });
+        imageCard.appendChild(img);
+        imageContainer.appendChild(imageCard);
+    });
 };
 
 (async () => {
-  const images = await fetchData();
-  displayImages(images);
+    const images = await fetchData();
+    displayImages(images);
 })();
